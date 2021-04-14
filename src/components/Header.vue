94480f5bb7c1d81f3d78935af8f1aabaa9a57906
Allan ROLLE <allan.rolle@laposte.net>
2021-04-14 21:31:11 +0200

    Add header

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <a href="" class="logo-materiel-link">
+        <img class='logo-materiel' src='https://media.materiel.net/cms/2021-01/site-logo-white.svg' alt=''>
+      </a>
+      <img class='logo-vue' src='../assets/logo.png'>
+    </div>  
+  </header>
+</template>
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+<style scoped>
+ .header{
+   background: linear-gradient(90deg,#353535 0,#515151 20%,#515151 46%,#353535);
+ }
+ .container{
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   max-width: 1340px;
+   margin: 0 auto;
+ }
+ .logo-materiel-link{
+   width: 100%;
+ }
+ .logo-materiel{
+   width: 30%;
+ }
+ .logo-vue{
+   height: auto;
+   width: 50px;
+ }
+</style>
